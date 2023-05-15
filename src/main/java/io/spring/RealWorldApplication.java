c95f3e0486fa5b1ed1be63c34d1a37ea7c929757
Oleksii Tereshchenko <o.tereshchenko@spd-ukraine.com>
2023-05-15 12:25:06 +0300

    test

--- src/main/java/io/spring/RealWorldApplication.java
+++ src/main/java/io/spring/RealWorldApplication.java
@@ -9,6 +9,6 @@ public class RealWorldApplication {
   public static void main(String[] args) {
     String test = "test";
     System.out.println("only for test");
-    SpringApplication.run(RealWorldApplication.class, args);
+    SpringApplication.run(RealWorldApplication.class, ar
   }
-}
+
